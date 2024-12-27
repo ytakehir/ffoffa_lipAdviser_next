e97a9324d052ddfe4a9be5c2e2348abad890673f
ytakehir <takegryffindor@gmail.com>
2024-12-27 17:46:08 +0900

    商用対応

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  distDir: './dist', // ビルドの出力ディレクトリを `./dist/` に変更します
   basePath: process.env.NEXT_PUBLIC_BASE_PATH, // basePath を `/` に設定します
   reactStrictMode: true,
   eslint: {
